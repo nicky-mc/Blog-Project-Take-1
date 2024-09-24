04fe5588f2dcdf1350afaa3e5f02d9767790b338
nicky-mc <cinimod@gmail.com>
2024-09-24 17:26:57 +0100

    skeleton built

---
+++ public/js/firebase.js
@@ -0,0 +1,7 @@
+<>
+  <script src="https://www.gstatic.com/firebasejs/8.9.1/firebase-app.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/8.9.1/firebase-firestore.js"></script>
+  <script src="js/firebase.js"></script>
+  <script src="js/editor.js"></script>
+  <script src="js/blog.js"></script>
+</>;
